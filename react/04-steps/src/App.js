7fd6ad99325411408dbcf41cd050c5b9c7f4c931
Xiang Zhang <fancyzhx@gmail.com>
2023-10-18 23:08:12 -0400

    Completed Section 6: State, Events, and Forms: Interactive Components

---
+++ react/04-steps/src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+const messages = ['Learn React', 'Apply for jobs', 'Invest your new income'];
+
+export default function App() {
+  return (
+    <div className="App">
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    setStep((step) => (step > 1 ? step - 1 : 1));
+  }
+
+  function handleNext() {
+    setStep((step) => (step < 3 ? step + 1 : 3));
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
