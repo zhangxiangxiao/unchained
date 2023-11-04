5b3c7d76883a5b1ea3419c53cfa53ad559588735
Xiang Zhang <fancyzhx@gmail.com>
2023-11-05 00:36:34 +0800

    Completed Section 11: How React Works Behind the Scenes

--- react/06-eat-n-split/src/App.js
+++ react/06-eat-n-split/src/App.js
@@ -68,6 +68,7 @@ export default function App() {
       </div>
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
